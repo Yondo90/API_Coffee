77be0bf1333e6e13c331ee8d8032b5ec43fd2b22
Yondo90 <yondo@gmail.com>
2024-09-26 16:53:36 +0300

    first commit incompleted

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,249 @@
+function App() {
+  return (
+    <div className="bg-[#111315] bg-cover  relative w-full h-[100vh]">
+      <div className=" w-full sm:w-full h-48 bg-cover ">
+        <img
+          className=" h-48 w-full sm:w-full "
+          src="public\bg-cafe.jpg"
+          alt="photo cafe"
+        />
+      </div>
+      <div className=" w-full flex justify-center bg-[#111315] h-[803px] ">
+        <div className="bg-[#1B1D1F] bg-[url(vector.svg)] bg-no-repeat bg-right-top bg mx-auto h-[800px] p-16 w-3/4 absolute -mt-24 rounded-lg  ">
+          <div className=" w-3/5 mx-auto ">
+            <h1 className="mx-auto font-bold text-xl text-[#FEF7EE] flex items-center justify-center">
+              Our Collection
+            </h1>
+            <p className="text-center text-md text-[#6F757C] ">
+              Introducing our Coffee Collection, a selection of unique coffees
+              from different roast types and origins, expertly roasted in small
+              batches and shipped fresh weekly
+            </p>
+          </div>
+          <div className="flex items-center justify-center space-x-3 mt-6 ">
+            <div className="bg-[#6F757C] h-10 w-28 p-3 pt-2 pb-2 rounded-lg">
+              All Products
+            </div>
+            <div className="bg-[#6F757C] h-10 w-32 p-3 pt-2 pb-2 rounded-lg">
+              Available Now
+            </div>
+          </div>
+          <div className="grid grid-cols-3 ">
+            <div className=" w-full mt-4 h-40 ">
+              <div className=" w-60 h-40 rounded-lg ">
+                <img
+                  className="w-60 h-40 rounded-lg"
+                  src="public\coffee\coffee1.jpg"
+                  alt="photo coffee"
+                />
+                <div className="bg-[#F6C768] p-1 font-semibold rounded-full pl-2 pr-2 w-20 flex items-center justify-center text-xs absolute -mt-36 ml-3 ">
+                  Popular
+                </div>
+                <div className=" h-10 ">
+                  <div className="flex items-center mt-2 justify-between">
+                    <div className="bg-[#1B1D1F] p-1 text-[#FEF7EE] font-semibold">
+                      Cappuccino
+                    </div>
+                    <div className="bg-[#BEE3CC] p-1 rounded-lg text-center">
+                      $5.20
+                    </div>
+                  </div>
+                  <div className="bg-[#1B1D1F] flex items-center space-x-2 mt-2 text-[#FEF7EE]">
+                    <img
+                      src="public\Star_fill.svg"
+                      className="h-5 "
+                      alt="icon etoile"
+                    />
+                    <p className="text-xs text-center">
+                      5.0
+                      <span className=" text-xs text-[#6F757C] ">
+                        (20 votes)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" w-full mt-4 h-40 ">
+              <div className=" w-60 h-40 rounded-lg ">
+                <img
+                  className="w-60 h-40 rounded-lg"
+                  src="public\coffee\coffee1.jpg"
+                  alt="photo coffee"
+                />
+                <div className="bg-[#F6C768] p-1 font-semibold rounded-full pl-2 pr-2 w-20 flex items-center justify-center text-xs absolute -mt-36 ml-3 ">
+                  Popular
+                </div>
+                <div className=" h-10 ">
+                  <div className="flex items-center mt-2 justify-between">
+                    <div className="bg-[#1B1D1F] p-1 text-[#FEF7EE] font-semibold">
+                      Cappuccino
+                    </div>
+                    <div className="bg-[#BEE3CC] p-1 rounded-lg text-center">
+                      $5.20
+                    </div>
+                  </div>
+                  <div className="bg-[#1B1D1F] flex items-center space-x-2 mt-2 text-[#FEF7EE]">
+                    <img
+                      src="public\Star_fill.svg"
+                      className="h-5 "
+                      alt="icon etoile"
+                    />
+                    <p className="text-xs text-center">
+                      5.0
+                      <span className=" text-xs text-[#6F757C] ">
+                        (20 votes)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" w-full mt-4 h-40 ">
+              <div className=" w-60 h-40 rounded-lg ">
+                <img
+                  className="w-60 h-40 rounded-lg"
+                  src="public\coffee\coffee1.jpg"
+                  alt="photo coffee"
+                />
+                <div className="bg-[#F6C768] p-1 font-semibold rounded-full pl-2 pr-2 w-20 flex items-center justify-center text-xs absolute -mt-36 ml-3 ">
+                  Popular
+                </div>
+                <div className=" h-10 ">
+                  <div className="flex items-center mt-2 justify-between">
+                    <div className="bg-[#1B1D1F] p-1 text-[#FEF7EE] font-semibold">
+                      Cappuccino
+                    </div>
+                    <div className="bg-[#BEE3CC] p-1 rounded-lg text-center">
+                      $5.20
+                    </div>
+                  </div>
+                  <div className="bg-[#1B1D1F] flex items-center space-x-2 mt-2 text-[#FEF7EE]">
+                    <img
+                      src="public\Star_fill.svg"
+                      className="h-5 "
+                      alt="icon etoile"
+                    />
+                    <p className="text-xs text-center">
+                      5.0
+                      <span className=" text-xs text-[#6F757C] ">
+                        (20 votes)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" w-full mt-28 h-40 ">
+              <div className=" w-60 h-40 rounded-lg ">
+                <img
+                  className="w-60 h-40 rounded-lg"
+                  src="public\coffee\coffee1.jpg"
+                  alt="photo coffee"
+                />
+                <div className="bg-[#F6C768] p-1 font-semibold rounded-full pl-2 pr-2 w-20 flex items-center justify-center text-xs absolute -mt-36 ml-3 ">
+                  Popular
+                </div>
+                <div className=" h-10 ">
+                  <div className="flex items-center mt-2 justify-between">
+                    <div className="bg-[#1B1D1F] p-1 text-[#FEF7EE] font-semibold">
+                      Cappuccino
+                    </div>
+                    <div className="bg-[#BEE3CC] p-1 rounded-lg text-center">
+                      $5.20
+                    </div>
+                  </div>
+                  <div className="bg-[#1B1D1F] flex items-center space-x-2 mt-2 text-[#FEF7EE]">
+                    <img
+                      src="public\Star_fill.svg"
+                      className="h-5 "
+                      alt="icon etoile"
+                    />
+                    <p className="text-xs text-center">
+                      5.0
+                      <span className=" text-xs text-[#6F757C] ">
+                        (20 votes)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" w-full mt-28 h-40 ">
+              <div className=" w-60 h-40 rounded-lg ">
+                <img
+                  className="w-60 h-40 rounded-lg"
+                  src="public\coffee\coffee1.jpg"
+                  alt="photo coffee"
+                />
+                <div className="bg-[#F6C768] p-1 font-semibold rounded-full pl-2 pr-2 w-20 flex items-center justify-center text-xs absolute -mt-36 ml-3 ">
+                  Popular
+                </div>
+                <div className=" h-10 ">
+                  <div className="flex items-center mt-2 justify-between">
+                    <div className="bg-[#1B1D1F] p-1 text-[#FEF7EE] font-semibold">
+                      Cappuccino
+                    </div>
+                    <div className="bg-[#BEE3CC] p-1 rounded-lg text-center">
+                      $5.20
+                    </div>
+                  </div>
+                  <div className="bg-[#1B1D1F] flex items-center space-x-2 mt-2 text-[#FEF7EE]">
+                    <img
+                      src="public\Star_fill.svg"
+                      className="h-5 "
+                      alt="icon etoile"
+                    />
+                    <p className="text-xs text-center">
+                      5.0
+                      <span className=" text-xs text-[#6F757C] ">
+                        (20 votes)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" w-full mt-28 h-40 ">
+              <div className=" w-60 h-40 rounded-lg ">
+                <img
+                  className="w-60 h-40 rounded-lg"
+                  src="public\coffee\coffee1.jpg"
+                  alt="photo coffee"
+                />
+                <div className="bg-[#F6C768] p-1 font-semibold rounded-full pl-2 pr-2 w-20 flex items-center justify-center text-xs absolute -mt-36 ml-3 ">
+                  Popular
+                </div>
+                <div className=" h-10 ">
+                  <div className="flex items-center mt-2 justify-between">
+                    <div className="bg-[#1B1D1F] p-1 text-[#FEF7EE] font-semibold">
+                      Cappuccino
+                    </div>
+                    <div className="bg-[#BEE3CC] p-1 rounded-lg text-center">
+                      $5.20
+                    </div>
+                  </div>
+                  <div className="bg-[#1B1D1F] flex items-center space-x-2 mt-2 text-[#FEF7EE]">
+                    <img
+                      src="public\Star_fill.svg"
+                      className="h-5 "
+                      alt="icon etoile"
+                    />
+                    <p className="text-xs text-center">
+                      5.0
+                      <span className=" text-xs text-[#6F757C] ">
+                        (20 votes)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
